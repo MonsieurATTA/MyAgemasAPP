900d5092ead534f8fd291c763fe38591c266f05f
MonsieurATTA <ATTA Loic>
2025-10-29 16:49:05 +0000

    modification

---
+++ lib/Pages/home/widgets/Produits.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Produits extends StatelessWidget {
+  const Produits({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(height: 150, color: Colors.blue);
+  }
+}
--- lib/Pages/home/widgets/category.dart
+++ lib/Pages/home/widgets/category.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:myagemas/Pages/home/widgets/Produits.dart';
 
 class Category extends StatelessWidget {
   Category({super.key});
 
   final categories = [
     {
-      'icon': Icons.person_3_sharp,
+      'icon': Icons.person_4_outlined,
       'label': 'Adhérents',
       'color': Color(0xFF605CF4),
     },
     {
-      'icon': Icons.people_outline,
+      'icon': Icons.people_outline_sharp,
       'label': 'Bénéficiaires',
       'color': Color(0xFFFC77A6),
     },
@@ -27,6 +28,7 @@ class Category extends StatelessWidget {
   ];
 
   @override
+  // L'arrièere-plan de la section catégorie en blanc avec des coins arrondis en haut
   Widget build(BuildContext context) {
     return Container(
       height: 420,
@@ -36,6 +38,58 @@ class Category extends StatelessWidget {
           topLeft: Radius.circular(20),
           topRight: Radius.circular(20),
         ),
+      ),
+      // Le contenu de la section catégorie
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 140,
+            /* color: Colors.blueGrey, */
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(
+                horizontal: 25,
+              ), // Especment à gauche à droite
+              scrollDirection: Axis.horizontal,
+              itemBuilder: ((context, index) => Column(
+                children: [
+                  const SizedBox(height: 25),
+                  Container(
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: categories[index]['color'] as Color,
+                    ),
+                    child: Icon(
+                      categories[index]['icon'] as IconData,
+                      color: Colors.white,
+                      size: 40,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    categories[index]['label'] as String,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              )),
+              separatorBuilder: ((context, index) => const SizedBox(width: 33)),
+              itemCount: categories.length,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: const Text(
+              'retrouvez les pharmacies',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Produits(),
+        ],
       ),
     );
   }
